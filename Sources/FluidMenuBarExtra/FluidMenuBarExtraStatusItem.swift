ec1413eac65e3b9e5115f0874bf7557f2d2d0fdb
Adam Różyński <adam@micropixels.pl>
2024-03-15 12:00:39 +0100

    Remove not needed print

--- Sources/FluidMenuBarExtra/FluidMenuBarExtraStatusItem.swift
+++ Sources/FluidMenuBarExtra/FluidMenuBarExtraStatusItem.swift
@@ -73,7 +73,6 @@ public final class FluidMenuBarExtraStatusItem: NSObject {
     }
 
     private func didPressStatusBarButton(_: NSStatusBarButton) {
-        print(#function)
         toggleWindow()
     }
 
